e5898db8423baacb34b21b021dc70f8fc28bd341
Harshad Reddy Nalla <hnalla@redhat.com>
2026-04-06 11:21:43 -0400

    Update @odh-dashboard/notebooks: feat: Add PVC and StorageClasses API client to frontend (#934)
    Upstream commit: 697ffd7f76e2f8ffd61ae00b6069f5e6c9f52f98

--- packages/notebooks/upstream/workspaces/frontend/src/generated/Workspacekinds.ts
+++ packages/notebooks/upstream/workspaces/frontend/src/generated/Workspacekinds.ts
@@ -20,7 +20,7 @@ import { ContentType, HttpClient, RequestParams } from './http-client';
 
 export class Workspacekinds<SecurityDataType = unknown> extends HttpClient<SecurityDataType> {
   /**
-   * @description Returns a list of all workspace kinds in the cluster. When namespaceFilter is provided, authorization checks whether the user can create workspaces in that namespace instead of requiring workspace kind list permission.
+   * @description Returns a list of all workspace kinds in the cluster.
    *
    * @tags workspacekinds
    * @name ListWorkspaceKinds
@@ -73,6 +73,7 @@ export class Workspacekinds<SecurityDataType = unknown> extends HttpClient<Secur
       ...params,
     });
   /**
+   * @description Returns details of a specific workspace kind identified by its name.
    * @description Returns details of a specific workspace kind identified by its name.
    *
    * @tags workspacekinds
